ba1960359b8894e629e3892fd44a667060052c3e
vborisuk <vladimirborisiuk@gmail.com>
2023-05-17 10:59:48 +0300

    Features Gallery Module

---
+++ Modules/Gallery/Http/Controllers/Admin/GalleryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Gallery\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class GalleryController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function create()
+    {
+
+    }
+    public function store(Request $request)
+    {
+        //
+    }
+    public function show($id)
+    {
+
+    }
+    public function edit($id)
+    {
+
+    }
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    public function destroy($id)
+    {
+        //
+    }
+}
