972e8c52cd20b055157eeefa4f7f03b0d4e6d437
KRordel <romkakordel@gmail.com>
2023-05-18 10:35:43 +0300

    Добавил Priline (необходим npm install) Принудительно отключил DarkMode в tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        'node_modules/preline/dist/*.js',
     ],
+    darkMode: 'class',
     theme: {
         extend: {},
     },
     plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
+        require('preline/plugin'),
     ],
 }
