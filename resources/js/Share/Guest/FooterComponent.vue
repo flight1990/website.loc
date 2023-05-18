ece4bc99d89ba1abfa9b75c92f255189a49cabeb
vborisuk <vladimirborisiuk@gmail.com>
2023-05-18 11:55:11 +0300

    Features Добавил футер на госте и вывел в качестве примера телефон, адрес, емайл

---
+++ resources/js/Share/Guest/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Footer Here...
+
+        <ul>
+            <li>Телефон: {{ $page.props.site_settings.phone }}</li>
+            <li>Email: {{ $page.props.site_settings.email }}</li>
+            <li>Адрес: {{ $page.props.site_settings.address }}</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterComponent"
+}
+</script>
