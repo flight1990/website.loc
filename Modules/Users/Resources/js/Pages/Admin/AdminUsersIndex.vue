c4435109891dcdd94244be94be5600316d8f67af
KRordel <romkakordel@gmail.com>
2023-05-22 16:53:01 +0300

    Верстка ПОЛЬЗОВАТЕЛИ в админке

--- Modules/Users/Resources/js/Pages/Admin/AdminUsersIndex.vue
+++ Modules/Users/Resources/js/Pages/Admin/AdminUsersIndex.vue
@@ -1,9 +1,11 @@
 <template>
-    <h1>Пользовтелями</h1>
 
-    <inertia-link href="/admin/users/create">
-        Создать нового пользователя
-    </inertia-link>
+    <div class="flex items-center justify-between flex-wrap gap-6 mb-10">
+        <h1 class="block text-2xl font-bold text-gray-800 sm:text-3xl">Пользователи</h1>
+        <inertia-link href="/admin/users/create" class="solid-button-primary">
+            Создать пользователя
+        </inertia-link>
+    </div>
 
     <div>
         <table id="table" class="display" style="width: 100%;" v-once>
@@ -40,6 +42,23 @@ export default {
                 {data: 'email', name: 'email'},
                 {data: 'actions', name: 'actions', orderable: false, searchable: false},
             ],
+            scrollX: true,
+            lengthMenu: [
+                [10, 25, 50, -1],
+                [10, 25, 50, 'Все'],
+            ],
+            language: {
+                "emptyTable": "Данные отсутствуют",
+                "infoEmpty": "",
+                "info": "C _START_ по _END_ из _TOTAL_",
+                "lengthMenu": " _MENU_",
+                "search": "Поиск: ",
+                "zeroRecords": "Совпадающих записей не найдено",
+                'paginate': {
+                    'previous': '❮',
+                    'next': '❯'
+                },
+            },
             fnDrawCallback: function () {
                 replaceHtmlLinksToInertiaLinks('#table');
             }
--- Modules/Users/Resources/js/Pages/Admin/AdminUsersModify.vue
+++ Modules/Users/Resources/js/Pages/Admin/AdminUsersModify.vue
@@ -1,39 +1,43 @@
 <template>
-    <h1>{{ user ? 'Редактировать' : 'Создать нового' }} пользователя</h1>
+
+    <div class="flex items-center justify-between flex-wrap gap-6 mb-10">
+        <h1 class="block text-2xl font-bold text-gray-800 sm:text-3xl">{{ user ? 'Редактировать' : 'Создать нового' }} пользователя</h1>
+        <inertia-link href="/admin/users" class="soft-button-gray">
+            <svg class="h-6" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 9h11a4 4 0 1 1 0 8h-1"></path>
+                <path d="M9 13 5 9l4-4"></path>
+            </svg>
+            Вернуться назад
+        </inertia-link>
+    </div>
 
     <form>
-
-        <h3>Информация пользователя</h3>
-
         <div>
-            <label for="name">ФИО</label>
-            <input type="text" id="name" v-model="form.name">
-            <small>{{ form.errors.name }}</small>
+            <label for="name" class="block text-sm font-medium mb-2">Имя</label>
+            <input type="text" id="name" v-model="form.name" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-primary-500 focus:ring-primary-500">
+            <p class="text-sm text-rose-500 mb-2">{{ form.errors.name }}</p>
         </div>
 
         <div>
-            <label for="email">Электронный адрес</label>
-            <input type="text" id="email" v-model="form.email">
-            <small>{{ form.errors.email }}</small>
+            <label for="email" class="block text-sm font-medium mb-2">Электронный адрес</label>
+            <input type="email" id="email" v-model="form.email" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-primary-500 focus:ring-primary-500">
+            <p class="text-sm text-rose-500 mb-2">{{ form.errors.email }}</p>
         </div>
 
         <div>
-            <label for="password">Пароль</label>
-            <input type="password" id="password" v-model="form.password">
-            <small>{{ form.errors.password }}</small>
+            <label for="password" class="block text-sm font-medium mb-2">Пароль</label>
+            <input type="password" id="password" v-model="form.password" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-primary-500 focus:ring-primary-500">
+            <p class="text-sm text-rose-500 mb-2">{{ form.errors.password }}</p>
         </div>
 
         <div>
-            <label for="password_confirmation">Подтверждение пароля</label>
-            <input type="password" id="password_confirmation" v-model="form.password_confirmation">
-            <small>{{ form.errors.password_confirmation }}</small>
+            <label for="password_confirmation" class="block text-sm font-medium mb-2">Подтверждение пароля</label>
+            <input type="password" id="password_confirmation" v-model="form.password_confirmation" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-primary-500 focus:ring-primary-500">
+            <p class="text-sm text-rose-500 mb-2">{{ form.errors.password_confirmation }}</p>
         </div>
 
-        <button :disabled="form.processing" @click.prevent="submitHandler">
-            {{ user ? 'Обновить' : 'Сохранить' }}
-        </button>
+        <button class="solid-button-primary disabled:bg-gray-500" :disabled="form.processing" @click.prevent="submitHandler">{{ user ? 'Обновить' : 'Сохранить' }}</button>
 
-        <inertia-link href="/admin/users">Вернуться к списку</inertia-link>
     </form>
 </template>
 
