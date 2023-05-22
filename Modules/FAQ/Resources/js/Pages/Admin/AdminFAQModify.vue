a5456b4f53d1440616e4e984cea217c00a380469
KRordel <romkakordel@gmail.com>
2023-05-22 16:25:57 +0300

    Верстка FAQ в админке

--- Modules/FAQ/Resources/js/Pages/Admin/AdminFAQModify.vue
+++ Modules/FAQ/Resources/js/Pages/Admin/AdminFAQModify.vue
@@ -1,40 +1,38 @@
 <template>
-    <h1>{{ faq ? 'Редактировать' : 'Создать новое' }} FAQ</h1>
 
-    <form>
+    <div class="flex items-center justify-between flex-wrap gap-6 mb-10">
+        <h1 class="block text-2xl font-bold text-gray-800 sm:text-3xl">{{ faq ? 'Редактировать' : 'Создать' }} вопрос-ответ</h1>
+        <inertia-link href="/admin/faq" class="soft-button-gray">
+            <svg class="h-6" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 9h11a4 4 0 1 1 0 8h-1"></path>
+                <path d="M9 13 5 9l4-4"></path>
+            </svg>
+            Вернуться назад
+        </inertia-link>
+    </div>
+
+    <form class="space-y-6">
 
         <div>
-            <label for="question">Вопрос</label>
-            <input type="text" id="question" v-model="form.question">
-            <small>{{ form.errors.question }}</small>
+            <label for="question" class="block text-sm font-medium mb-2">Вопрос</label>
+            <input type="text" id="question"  v-model="form.question" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-primary-500 focus:ring-primary-500">
+            <p class="text-sm text-rose-500 mb-2">{{ form.errors.question }}</p>
         </div>
 
         <div>
-            <label for="answer">Ответ</label>
-            <simple-tiny-editor
-                id="answer"
-                v-model="form.answer"
-            />
-            <small>{{ form.errors.answer }}</small>
+            <label for="title" class="block text-sm font-medium mb-2">Ответ</label>
+            <simple-tiny-editor id="answer" v-model="form.answer" />
+            <p class="text-sm text-rose-500 mb-2">{{ form.errors.answer }}</p>
         </div>
 
-        <div>
-            <label for="is_active">Статус ({{ form.is_active ? 'Активен' : 'Не активен' }})</label>
-            <input type="checkbox" id="is_active" v-model="form.is_active">
-            <small>{{ form.errors.is_active }}</small>
+        <div class="flex items-center">
+            <input type="checkbox" id="is_active" v-model="form.is_active" class="relative shrink-0 w-[3.25rem] h-7 bg-gray-200 checked:bg-none checked:bg-primary-600 checked:hover:bg-primary-700 checked:focus:bg-primary-700 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 border border-transparent ring-1 ring-transparent focus:border-primary-600 focus:ring-primary-600 ring-offset-white focus:outline-none appearance-none before:inline-block before:w-6 before:h-6 before:bg-white checked:before:bg-primary-200 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200">
+            <label for="is_active" class="text-sm font-medium ml-3 select-none">Статус ({{ form.is_active ? 'Активна' : 'Не активна' }})</label>
         </div>
 
-        <button
-            :disabled="form.processing"
-            @click.prevent="submitHandler"
-        >
-            {{ faq ? 'Обновить' : 'Сохранить' }}
-        </button>
+        <button class="solid-button-primary disabled:bg-gray-500" :disabled="form.processing" @click.prevent="submitHandler">{{ faq ? 'Обновить' : 'Сохранить' }}</button>
+
     </form>
-
-    <inertia-link href="/admin/faq">
-        Вернуться назад
-    </inertia-link>
 
 </template>
 
