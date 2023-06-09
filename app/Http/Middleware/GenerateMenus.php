175fec712bb444cf0cec8e645dc67a3791222494
vborisuk <vladimirborisiuk@gmail.com>
2023-06-09 09:24:38 +0300

    Final Fixes

--- app/Http/Middleware/GenerateMenus.php
+++ app/Http/Middleware/GenerateMenus.php
@@ -22,7 +22,6 @@ class GenerateMenus
                 $menu->add('Меню', route('admin.menus.index'))->active('admin/menus/*');
                 $menu->add('Пользователи', route('admin.users.index'))->active('admin/users/*');
                 $menu->add('Настройки', route('admin.settings.index'))->active('admin/settings/*');
-//                $menu->add('Вернуться к сайту', route('guest.pages.index'));
             }
         });
     }
@@ -33,9 +32,7 @@ class GenerateMenus
             $menu->add('Главная', route('guest.pages.index'));
             app(BuildMenusFromDataBaseAction::class)->run($menu);
             $menu->add('Галерея', route('guest.gallery.index'))->active('gallery/*');
-
             $menu->add('Контакты', route('guest.pages.index').'#contact');
-//            $menu->add('Панель управления', route('admin.index'));
         });
     }
 
